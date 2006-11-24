87dc34af5a2c18bd2606a46a665323cddc9743cd
Alec Mitchell <apm13@columbia.edu>
2006-11-24 23:25:14 +0000

    ZCMLLayer was breaking tests, I'm not sure if I care to know why.
    svn path=/CMFDiffTool/trunk/; revision=34091

--- Products/CMFDiffTool/tests/BaseTestCase.py
+++ Products/CMFDiffTool/tests/BaseTestCase.py
@@ -1,7 +1,6 @@
 # BaseTestCase
 
 from Products.CMFTestCase.ctc import *
-from Products.CMFTestCase.layer import ZCMLLayer
 
 installProduct('CMFDiffTool')
 
@@ -11,4 +10,3 @@ class BaseTestCase(CMFTestCase):
     """This is a stub now, but in case you want to try
        something fancy on Your Branch (tm), put it here.
     """
-    layer = ZCMLLayer
