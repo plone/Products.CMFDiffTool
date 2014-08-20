a4aecb0cf67e2af1db48c58df77886e3b7f820a6
Tom Gross <itconsense@gmail.com>
2014-08-20 14:46:49 +0200

    use not deprecated test methods

--- Products/CMFDiffTool/tests/testDiffTool.py
+++ Products/CMFDiffTool/tests/testDiffTool.py
@@ -16,7 +16,9 @@ class DummyDiff:
 class DummyDiff2:
     meta_type = "Second Dummy Diff Type"
 
-class TestDiffTool(BaseTestCase.BaseTestCase):
+from plone.app.testing.bbb import PloneTestCase
+
+class TestDiffTool(PloneTestCase):
     """Test the portal_diff tool"""
 
     def afterSetUp(self):
@@ -26,20 +28,20 @@ class TestDiffTool(BaseTestCase.BaseTestCase):
     def testInterface(self):
         """Ensure that tool instances implement the portal_diff interface"""
         from Products.CMFDiffTool.interfaces.portal_diff import portal_diff
-        self.failUnless(portal_diff.providedBy(self.p_diff))
+        self.assertTrue(portal_diff.providedBy(self.p_diff))
 
     def testRegisterDiffType(self):
         """Test registration of Diff types"""
         unregisterDiffType(DummyDiff)
-        self.failIf('Dummy Diff Type' in self.p_diff.listDiffTypes())
+        self.assertNotIn('Dummy Diff Type', self.p_diff.listDiffTypes())
         registerDiffType(DummyDiff)
-        self.failUnless('Dummy Diff Type' in self.p_diff.listDiffTypes())
+        self.assertIn('Dummy Diff Type', self.p_diff.listDiffTypes())
 
     def testSetDiff(self):
         """Test setDiffForPortalType() method"""
         d = {'field1':'TestDiff', 'field2':'Dummy Diff Type'}
         self.p_diff.setDiffForPortalType('Document', d)
-        self.failUnless(self.p_diff.getDiffForPortalType('Document') == d)
+        self.assertEqual(self.p_diff.getDiffForPortalType('Document'), d)
 
     def testSetDiffReplaces(self):
         """Test that setDiffForPortalType() replaces old data"""
@@ -47,19 +49,20 @@ class TestDiffTool(BaseTestCase.BaseTestCase):
         d2 = {'field3':'Dummy Diff Type'}
         self.p_diff.setDiffForPortalType('Document', d1)
         self.p_diff.setDiffForPortalType('Document', d2)
-        self.failUnless(self.p_diff.getDiffForPortalType('Document') == d2)
+        self.assertEqual(self.p_diff.getDiffForPortalType('Document'), d2)
 
     def testSingleSetDiffField(self):
         """Test setDiffField method"""
         self.p_diff.setDiffField('Document', 'title', 'Dummy Diff Type')
-        self.failUnless(self.p_diff.getDiffForPortalType('Document') == {'title':'Dummy Diff Type'})
+        self.assertEqual(self.p_diff.getDiffForPortalType('Document'),
+                         {'title':'Dummy Diff Type'})
 
     def testMultipleSetDiffField(self):
         """Test setDiffField method adding a second field to one content type"""
         self.p_diff.setDiffField('Document', 'title', 'Dummy Diff Type')
         self.p_diff.setDiffField('Document', 'description', 'Dummy Diff Type')
         d = {'title':'Dummy Diff Type', 'description':'Dummy Diff Type'}
-        self.failUnless(self.p_diff.getDiffForPortalType('Document') == d)
+        self.assertEqual(self.p_diff.getDiffForPortalType('Document'), d)
 
     def testReplaceSetDiffField(self):
         """Test that setDiffField does a replace for existing fields"""
@@ -67,7 +70,7 @@ class TestDiffTool(BaseTestCase.BaseTestCase):
         self.p_diff.setDiffField('Document', 'title', 'Dummy Diff Type')
         self.p_diff.setDiffField('Document', 'title', 'Second Dummy Diff Type')
         d = {'title':'Second Dummy Diff Type'}
-        self.failUnless(self.p_diff.getDiffForPortalType('Document') == d)
+        self.assertEqual(self.p_diff.getDiffForPortalType('Document'), d)
         unregisterDiffType(DummyDiff2)
 
     def testSetDiffFieldNameFailure(self):
