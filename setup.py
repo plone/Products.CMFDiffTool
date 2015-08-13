a37f0ea9f25c07bbe4c8eadfd564608769342f07
Maurits van Rees <maurits@vanrees.org>
2015-08-13 17:24:09 +0200

    Cleanup setup.py.
    pep8.
    More classifiers.
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,15 +2,22 @@
 from setuptools import setup, find_packages
 
 version = '2.1.1.dev0'
+long_description = open("README.rst").read()
+long_description += "\n"
+long_description += open("CHANGES.rst").read()
 
 setup(name='Products.CMFDiffTool',
       version=version,
       description="Diff tool for Plone",
-      long_description=open("README.rst").read() + "\n" +
-                       open("CHANGES.rst").read(),
+      long_description=long_description,
       classifiers=[
-        "Framework :: Plone",
-        ],
+          'Development Status :: 5 - Production/Stable',
+          'Framework :: Plone',
+          'Framework :: Plone :: 4.3',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          ],
       keywords='Diff Plone',
       author='Brent Hendricks',
       author_email='plone-developers@lists.sourceforge.net',
@@ -21,20 +28,20 @@ setup(name='Products.CMFDiffTool',
       include_package_data=True,
       zip_safe=False,
       extras_require=dict(
-        test=[
-            'zope.component',
-            'Products.CMFTestCase',
-            'plone.namedfile',
-            'plone.app.dexterity',
-        ]
+          test=[
+              'zope.component',
+              'Products.CMFTestCase',
+              'plone.namedfile',
+              'plone.app.dexterity',
+          ]
       ),
       install_requires=[
-        'setuptools',
-        'zope.interface',
-        'Products.CMFCore',
-        'Products.GenericSetup',
-        'Acquisition',
-        'Zope2',
+          'setuptools',
+          'zope.interface',
+          'Products.CMFCore',
+          'Products.GenericSetup',
+          'Acquisition',
+          'Zope2',
       ],
       entry_points="""
       [z3c.autoinclude.plugin]
