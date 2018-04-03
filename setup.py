ff42a2bc47da4014abd900252a6069ae4bb58500
Maurits van Rees <maurits@vanrees.org>
2018-04-03 22:56:37 +0200

    Updated classifiers: Plone 5.0 only.
    [ci skip]

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Framework :: Plone",
         "Framework :: Plone :: 5.0",
-        "Framework :: Plone :: 5.1",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Topic :: Internet :: WWW/HTTP",
     ],
