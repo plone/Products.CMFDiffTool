97bcbeabf46c054aae0a9aec242656af8b95a93a
Maurits van Rees <maurits@vanrees.org>
2023-03-01 00:13:45 +0100

    Add python_requires.
    [ci skip]

--- setup.py
+++ setup.py
@@ -15,11 +15,9 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Framework :: Plone",
         "Framework :: Plone :: 5.1",
         "Framework :: Plone :: 5.2",
-        "Framework :: Plone :: 6.0",
         "Framework :: Plone :: Core",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Topic :: Internet :: WWW/HTTP",
@@ -43,6 +41,7 @@ setup(
             'plone.app.robotframework',  # Only because plone.app.event fails
             ]
     ),
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*',
     install_requires=[
         'setuptools',
         'six',
